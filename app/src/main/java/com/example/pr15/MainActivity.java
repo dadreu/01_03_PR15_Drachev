82893a16831d131e654fd21972d5c7f292661e90
dadreu <dadreu@gmail.com>
2024-06-18 22:50:46 +0500

    Определение пути. Драчёв

--- app/src/main/java/com/example/pr15/MainActivity.java
+++ app/src/main/java/com/example/pr15/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pr15;
 
 import android.os.Bundle;
+import android.os.Environment;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +11,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String fileName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        fileName = Environment.getExternalStorageDirectory(Environment.DIRECTORY_DOCUMENTS) + "/records.3gpp";
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
